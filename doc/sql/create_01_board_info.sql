4ed647ae7be4df2d3dd5fec2a9adf584154096d5
ldh1103 <ldh517525@gmail.com>
2023-04-10 11:45:33 +0900

    23-04-10

---
+++ doc/sql/create_01_board_info.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE board;
+
+USE board;
+
+CREATE TABLE board_info (
+	board_no INT AUTO_INCREMENT PRIMARY KEY
+	,board_title VARCHAR(100) NOT NULL
+	,board_contents VARCHAR(1000) NOT NULL
+	,board_write_date DATETIME NOT NULL
+	,board_del_flg CHAR(1) NOT NULL DEFAULT ('0')
+	,board_del_date DATETIME
+);
+
+DESC board_info;
+
+COMMIT;
