5769c90142ff7c8c7df2286ef26558c886e8e713
ldh1103 <ldh517525@gmail.com>
2023-04-12 10:33:13 +0900

    23-04-12

--- src/board_update.php
+++ src/board_update.php
@@ -29,7 +29,7 @@
 
     // 내림차순 정렬일때 페이지 구하기
     ob_start(); include_once('board_list.php'); ob_end_clean(); // 다른 php파일 변수 불러오기 (다른내용은 출력 안함)
-    $list_page = ceil(( $result_cnt[0]["cnt"] - $result_info["board_no"] + 1 ) / $limit_num ); 
+    $list_page = ceil(( $result_cnt[0]["cnt"] - $result_info["board_no"] +1 ) / $limit_num ); 
     // 올림(( 전체글 갯수 - 현재글 번호 ) / 한페이지에 보여지는 글 수 );
 
     // 오름차순 정렬일때 페이지 구하기
